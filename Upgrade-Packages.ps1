7f8da41e65032030c6bcae03f7bc1570af245951
Carl-Oscar Hermansson <carl-oscar.hermansson@spsm.se>
2025-01-13 17:23:34 +0100

    Update Upgrade-Packages.ps1

--- Upgrade-Packages.ps1
+++ Upgrade-Packages.ps1
@@ -1,5 +1,5 @@
 ng update @angular/cli @angular/core
-ng update @angular/material @angular/cdk @angular-eslint/builder @angular-eslint/eslint-plugin @angular-eslint/eslint-plugin-template @angular-eslint/schematics @angular-eslint/template-parser
-ng update @ngneat/until-destroy @ngxs/devtools-plugin @ngxs/logger-plugin @ngxs/store
-ng update howler ngx-localstorage
+ng update @angular/material @angular/cdk @angular-eslint/builder @angular-eslint/eslint-plugin @angular-eslint/eslint-plugin-template @angular-eslint/schematics @angular-eslint/template-parser --force
+ng update @ngneat/until-destroy @ngxs/devtools-plugin @ngxs/logger-plugin @ngxs/store --force
+ng update howler ngx-localstorage --force
 #Check typescript for updates  ng update typescript@5.6.3
