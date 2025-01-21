2c0f723928f860ceb0f99a5068bb61fec36127dd
Carl-Oscar Hermansson <carl-oscar.hermansson@spsm.se>
2025-01-21 11:00:58 +0100

    Fix typo in ui-components public-api.ts

--- projects/ui-components/src/public-api.ts
+++ projects/ui-components/src/public-api.ts
@@ -6,7 +6,7 @@ export * from "./lib/spsm-footer/spsm-footer.component";
 export * from "./lib/button-icon/button-icon.component";
 export * from "./lib/loading-spinner/loading-spinner.component";
 export * from "./lib/celebrate/celebrate-canon/celebrate-canon.component";
-export * from "./lib/celebrate/confetti-canvas/confetti-canvas.component";
+export * from "./lib/celebrate/confetti-canon/confetti-canon.component";
 export * from "./lib/celebrate/ballons/ballons.component";
 export * from "./lib/celebrate/fireworks/fireworks.component";
 export * from "./lib/celebrate/sparkling-stars/sparkling-stars.component";
