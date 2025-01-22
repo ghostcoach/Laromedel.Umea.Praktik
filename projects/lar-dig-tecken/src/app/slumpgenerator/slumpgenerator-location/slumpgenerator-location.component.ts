16f6f23f75687827815fef1fc4d3808abe0560db
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2025-01-22 15:14:02 +0100

    added css to start button and card and added in slumpgenerator

--- projects/lar-dig-tecken/src/app/slumpgenerator/slumpgenerator-location/slumpgenerator-location.component.ts
+++ projects/lar-dig-tecken/src/app/slumpgenerator/slumpgenerator-location/slumpgenerator-location.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ChangeDetectionStrategy } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import { StartButtonComponent } from '../../start-button/start-button.component'
+import { CardComponent } from '../../card/card.component';
 
 
 @Component({
   selector: 'app-slumpgenerator-location',
-  imports: [CommonModule, StartButtonComponent],
+  imports: [CommonModule, StartButtonComponent, CardComponent],
   templateUrl: './slumpgenerator-location.component.html',
   styleUrl: './slumpgenerator-location.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- projects/lar-dig-tecken/src/app/start-button/start-button.component.ts
+++ projects/lar-dig-tecken/src/app/start-button/start-button.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy } from '@angular/core';
+
 
 @Component({
   selector: 'app-start-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './start-button.component.html',
-  styleUrl: './start-button.component.scss'
+  styleUrl: './start-button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StartButtonComponent {
   onStart(){
