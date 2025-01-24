7906eaa0250ea68efbd05793cfbb3464f355d792
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2025-01-24 14:39:29 +0100

    added interface for for file and bildbegrepp and a new card-content-component

--- projects/lar-dig-tecken/src/app/card/card.component.ts
+++ projects/lar-dig-tecken/src/app/card/card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
+import { CardContentComponent } from './card-content/card-content.component';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CardContentComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
